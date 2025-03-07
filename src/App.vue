cbf95985aeb9969be595051e52410176f3d7e02f
Bernardo Cadore <bernardo.cadore95@gmail.com>
2025-03-06 22:33:30 -0300

    Finalizando o exercicio.

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,62 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <div class="text-center my-3">
+      <h1 class="m-0">Calculadora Aritmética EBAC - VueJS</h1>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="d-flex justify-content-evenly mb-3">
+      <input
+        class="form-control w-25"
+        type="number"
+        v-model.number="num1"
+      />
+      <select class="form-select mx-3 w-25" v-model="operacao">
+        <option selected>Selecione uma Operação</option>
+        <option value="soma">+</option>
+        <option value="subtracao">-</option>
+        <option value="multiplicacao">x</option>
+        <option value="divisao">/</option>
+      </select>
+      <input
+        class="form-control w-25"
+        type="number"
+        v-model.number="num2"
+      />
+    </div>
+    <div class="text-center mb-3">
+      <h2>Resultado: {{ resultado }}</h2>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operacao: "Selecione uma Operação",
+    };
+  },
+  computed: {
+    resultado() {
+      switch (this.operacao) {
+        case "soma":
+          return this.num1 + this.num2;
+        case "subtracao":
+          return this.num1 - this.num2;
+        case "multiplicacao":
+          return this.num1 * this.num2;
+        case "divisao":
+          return this.num2 !== 0
+            ? this.num1 / this.num2
+            : "Erro. Divisão por zero não é permitido!";
+        default:
+          return 0;
+      }
+    },
+  },
+};
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style></style>
